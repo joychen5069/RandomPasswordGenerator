aa690b3a57a5165ef4e5390e70aea7fef1916b51
Joy Chen <joychen5069@gmail.com>
2020-03-05 14:18:40 -0500

    fixed function to include characters 8-128

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//create variable array for numbers 8-126 for password length
+var array = [];
+for (var i = 8; i <= 128; i++) {
+array.push(i);
+}
+console.log(array)
 // Write password to the #password input
 function writePassword(length) {
   console.log("it works")
@@ -9,19 +15,25 @@ function writePassword(length) {
 
   passwordText.value = password;
 
-  // var text = "abcdefghijklmnop"
-  // var pos = str.indexOf(d)
-  // Math.floor()
-
-  var UserNumber = prompt("How many characters do you want your password to have? (must be between 8 and 128 characters")
+  var UserNumber = prompt("How many characters do you want your password to have? (must be between 8 and 128 characters)")
     //if UserNumer is not an integer, repeat question
   //else continue to characters
-
+  if (UserNumber.indexOf(array)) {
     var lowerCase = confirm("Do you lower case letters?")
     var UpperCase = confirm("Do you want upper case letters?")
     var Num = confirm("Do you want numbers?")
     var Special = confirm("DO you want special characters?")
+    
+  }
+    else {
+      alert("That's not allowed. Try again")
+   
+    }
     //if user chooses none for all, then repeat loop-->stick inside For Loop
+  //If User selects all options
+  //   if (lowerCase = 1 && UpperCase = 1 && Num = 1 && Special = 1) {
+  //   lowerCase 
+  // } 
 
   //if lowerCase then choose UserNumber characteres from string.toLowerCase 
 
@@ -30,7 +42,7 @@ function writePassword(length) {
   //If NUm then choose UserNUmber characters from string
 
   //If Special then choose UserNUmber characters from stringg
-  if 
+   
 
 }
 
