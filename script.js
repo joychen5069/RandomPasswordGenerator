8e88725d7b898cd10f945294e61659509d14489f
Joy Chen <joychen5069@gmail.com>
2020-03-05 21:58:34 -0500

    added notes to clarify what each line of js does for future reference

--- script.js
+++ script.js
@@ -4,20 +4,21 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword(length) {
-  console.log("it works")
  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password; //not entirely sure what this is doing right now...
 
+  //variables for the password
 var validChars = []
 var lower = 'abcdefghijklmnopqrstuvwxyz'.split('')
 var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 var numberlist = '1234567890'.split('')
 var special =  ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'.split('')
 
+//start of the alert box/questions
   var Input = prompt("How many characters do you want your password to have? (must be between 8 and 128 characters)")
-  var UserNumber = parseInt(Input)
+  var UserNumber = parseInt(Input) //turns Input into an integer
 
     //if UserNumer is an integer between 8 and 128, run through prompts
   //else continue kick back and repeat
@@ -27,7 +28,7 @@ var special =  ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'.split('')
     var Num = confirm("Do you want numbers?")
     var Special = confirm("Do you want special characters?")
     
-    //if user chooses all, then spit out password with all characters
+    //if user chooses none, tell them to start over
     if (lowerCase != 1 && UpperCase != 1 && Num != 1 && Special != 1) {
       alert("You have to pick at least one condition");
     }
@@ -41,26 +42,27 @@ var special =  ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'.split('')
 
     // if user chooses upper case
       if (UpperCase == 1){
+        //add upper to validChars
       validChars = validChars.concat(upper)
     }
 
     // if userchooses numbers
     if (Num == 1){
+      //add numberlist to validChars
       validChars = validChars.concat(numberlist)
     }
 
      //if user chooses special characters
     if (Special == 1){
+      //add special characters to validChars
       validChars = validChars.concat(special)
     }
 
 console.log(validChars)
 
 
-
-
-
   }
+  //IF USER DECIDED TO PUT SOMETHING NOT BETWEEN 8 AND 128 AND/OR IS NOT AN INTEGER, KICK IT BACK
     else {
       alert("That's not allowed. Try again")
    
