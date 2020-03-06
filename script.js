31aeb16397bb045bddefd3fe91d1861b603d934c
Joy Chen <joychen5069@gmail.com>
2020-03-05 21:53:56 -0500

    changed the if statements so that i just had an empty array that i added items to depending on what the user confirmed

--- script.js
+++ script.js
@@ -10,8 +10,15 @@ function writePassword(length) {
 
   passwordText.value = password;
 
+var validChars = []
+var lower = 'abcdefghijklmnopqrstuvwxyz'.split('')
+var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+var numberlist = '1234567890'.split('')
+var special =  ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'.split('')
+
   var Input = prompt("How many characters do you want your password to have? (must be between 8 and 128 characters)")
   var UserNumber = parseInt(Input)
+
     //if UserNumer is an integer between 8 and 128, run through prompts
   //else continue kick back and repeat
   if (UserNumber >= 8 && UserNumber <= 128 && Number.isInteger(UserNumber)) {
@@ -19,58 +26,47 @@ function writePassword(length) {
     var UpperCase = confirm("Do you want upper case letters?")
     var Num = confirm("Do you want numbers?")
     var Special = confirm("Do you want special characters?")
+    
     //if user chooses all, then spit out password with all characters
     if (lowerCase != 1 && UpperCase != 1 && Num != 1 && Special != 1) {
       alert("You have to pick at least one condition");
     }
 
-    // if user only chooses lower case
-    else if (lowerCase == 1 && UpperCase != 1 && Num != 1 && Special != 1){
-      //only lower case password
-      //choose the UserNumber number of lower case letters and shuffle to make password
+    // if user chooses lower case
+      if (lowerCase == 1){
+     //add lower to empty validChars
+      validChars = validChars.concat(lower)
+       
     }
 
-    // if user only chooses upper case
-    else if (lowerCase != 1 && UpperCase == 1 && Num != 1 && Special != 1){
-
+    // if user chooses upper case
+      if (UpperCase == 1){
+      validChars = validChars.concat(upper)
     }
 
-    // if user only chooses numbers
-    else if (lowerCase != 1 && UpperCase != 1 && Num == 1 && Special != 1){
-
+    // if userchooses numbers
+    if (Num == 1){
+      validChars = validChars.concat(numberlist)
     }
 
-    //if user only chooses special characters
-    else if (lowerCase != 1 && UpperCase != 1 && Num != 1 && Special == 1){
-
+     //if user chooses special characters
+    if (Special == 1){
+      validChars = validChars.concat(special)
     }
+
+console.log(validChars)
+
+
+
 
 
   }
     else {
-      console.log(Number.isInteger(UserNumber))
       alert("That's not allowed. Try again")
    
     }
-    //if user chooses none for all, then repeat loop-->stick inside For Loop
-  //If User selects all options
-  //   if (lowerCase = 1 && UpperCase = 1 && Num = 1 && Special = 1) {
-  //   lowerCase 
-  // } 
-
-  //if lowerCase then choose UserNumber characteres from string.toLowerCase 
-
-  //if UpperCase then choose UserNUmber characters from string.ToUpperCase
-
-  //If NUm then choose UserNUmber characters from string
-
-  //If Special then choose UserNUmber characters from stringg
-   
-
-}
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
